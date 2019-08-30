dbeb7a10ff00257d7fea0be7ef3028081d72fbb7
Olle Fredriksson <fredriksson.olle@gmail.com>
2019-08-30 12:25:06 +0200

    Add a single Int test

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -1,20 +1,41 @@
-module Main exposing (..)
+module Main exposing (main)
 
+import Api
 import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Element exposing (..)
+import Element.Background as Background
+import Http
+import RemoteData exposing (RemoteData)
+
+
+type alias ErrorWithMetadata =
+    ( Http.Error, Maybe { metadata : Http.Metadata, body : String } )
+
+
+type alias WebData a =
+    RemoteData ErrorWithMetadata a
+
+
+type alias WebResult a =
+    Result ErrorWithMetadata a
+
 
 
 ---- MODEL ----
 
 
 type alias Model =
-    {}
+    { roundtripIntProgress : WebData Int
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { roundtripIntProgress = RemoteData.Loading }
+    , Cmd.batch
+        [ Cmd.map GotRoundtripIntResponse <| Api.postRoundtripInt 41
+        ]
+    )
 
 
 
@@ -22,24 +43,50 @@ init =
 
 
 type Msg
-    = NoOp
+    = GotRoundtripIntResponse (WebResult Int)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        GotRoundtripIntResponse result ->
+            ( { model | roundtripIntProgress = RemoteData.fromResult result }, Cmd.none )
 
 
 
 ---- VIEW ----
 
 
-view : Model -> Html Msg
+view : Model -> Element Msg
 view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+    column [ width fill, padding 30 ]
+        [ el
+            [ paddingXY 20 10
+            , Background.color <| remoteDataColor model.roundtripIntProgress 41
+            ]
+          <|
+            text "postRoundtripInt"
         ]
+
+
+remoteDataColor : RemoteData e a -> a -> Color
+remoteDataColor data expected =
+    case data of
+        RemoteData.NotAsked ->
+            rgb 1 1 1
+
+        RemoteData.Loading ->
+            rgb 1 1 0.5
+
+        RemoteData.Success actual ->
+            if expected == actual then
+                rgb 0.5 1 0.5
+
+            else
+                rgb 1 0.5 0.5
+
+        RemoteData.Failure _ ->
+            rgb 1 0.5 0.5
 
 
 
@@ -49,7 +96,7 @@ view model =
 main : Program () Model Msg
 main =
     Browser.element
-        { view = view
+        { view = layout [] << view
         , init = \_ -> init
         , update = update
         , subscriptions = always Sub.none
