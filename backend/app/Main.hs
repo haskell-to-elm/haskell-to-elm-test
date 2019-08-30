a0b9a1bf78efebab6fc384e409c5bc2302e1b682
Olle Fredriksson <fredriksson.olle@gmail.com>
2019-08-30 12:24:49 +0200

    Add cors

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -5,6 +5,7 @@ module Main where
 import Protolude
 
 import Network.Wai.Handler.Warp
+import Network.Wai.Middleware.Cors
 import Network.Wai.Middleware.RequestLogger
 import Servant.API
 import Servant.Server
@@ -14,7 +15,14 @@ import API
 main :: IO ()
 main = do
   putText "Starting server on port 8081"
-  run 8081 $ logStdout $ serve (Proxy :: Proxy API) server
+  run 8081 $
+    logStdout $
+    cors
+      (const $ Just simpleCorsResourcePolicy
+        { corsRequestHeaders = "authorization" : simpleHeaders
+        }
+      ) $
+    serve (Proxy :: Proxy API) server
 
 server :: Server API
 server =
